412f171552bf733b16b0f6512c7a55fc679f2a56
marcos <marcos@5adba155-f38a-43c0-aea0-d88c706dde45>
2011-08-26 21:12:51 +0000

    [Update] Cooperados Controller correção de métodos
    git-svn-id: https://svn.autogeral.com.br/code/JZebra@793 5adba155-f38a-43c0-aea0-d88c706dde45

--- test/br/com/jcomputacao/impressao/zebra/NovoTesteLPT1.java
+++ test/br/com/jcomputacao/impressao/zebra/NovoTesteLPT1.java
@@ -63,50 +63,19 @@ public class NovoTesteLPT1 {
 //            saida.println(comandoImpressora);
 
             ///////////////////////
-            saida.println("STX");
             saida.println("N");
-            saida.println("Q100,0");
-            saida.println("q5524");
-            saida.println("S5");
-            saida.println("D10");
-            saida.println("ZT");
-            saida.println("LO80,5,485,0");
-            saida.println("GG80,10, \"LOGORT1\"");
-            saida.println("I8,3,351");
-            saida.println("A10,10,0,1,1,1,N,\"Cooperativa   Mista   de   Trabalho  dos\"");
-            saida.println("A10,23,0,1,1,1,N,\"Motoristas  Autonomos  de  Taxi Especial\"");
-            saida.println("A10,36,0,1,1,1,N,\"de Sao Paulo - Radio-Taxi\"");
-            saida.println("A10,49,0,1,1,1,N,\"CNPJ:46.553.947/0001-20  CCM:8.179.517-3\"");
-            saida.println("A10,70,0,2,1,1,N,\"          VIA CONTABILIDADE        .\"");
-            saida.println("LO5,95,575,0");
-            saida.println("A10,110,0,2,1,2,N,\"4378315\"");
-            saida.println("B115,110,0,2,2,6,70,B,\"4378315\"");
+            saida.println(EtiquetaZebraTLP2844.getRepresentacaoAlturaEtiqueta(500, 30, 15));
+            saida.println(EtiquetaZebraTLP2844.getRepresentacaoLarguraEtiqueta(400));
+            saida.println(EtiquetaZebraTLP2844.getRepresentacaoVelocidadeImpressao(5));
+            saida.println(EtiquetaZebraTLP2844.getRepresentacaoDensidadeImpressao(10));
+            saida.println(EtiquetaZebraTLP2844.getRepresentacaoHorientacaoImpressao("T"));
+            saida.println(EtiquetaZebraTLP2844.getRepresentacaoCarachterSet(8, 3, 351));
+            saida.println(EtiquetaZebraTLP2844.getRepresentacaoLabel(50, 10, 0, 1, 1, 1, "ESTA EH UMA IMPRESSAO TESTE"));
+            saida.println(EtiquetaZebraTLP2844.getRepresentacaoCodigoBarras(50, 25, 0, 2, 2, 6, 70,  "256452"));
+            saida.println(EtiquetaZebraTLP2844.getRepresentacaoImprimir());
 
-            saida.println("A80,225,0,1,1,1,N,\"Recepcionista\"");
-            saida.println("A220,225,0,1,1,1,N,\"Ponto de partida\"");
-            saida.println("LO80,240,130,0");
-            saida.println("LO220,240,160,0");
-            saida.println("A80,250,0,1,1,1,N,\"TATI\"");
-            saida.println("A220,250,0,1,1,1,N,\"Aeroporto de Congonhas\"");
+//            imprimeSaida(saida);
 
-            saida.println("A80,295,0,1,1,1,N,\"Destino\"");
-            saida.println("LO80,310,80,0");
-            saida.println("A80,320,0,3,1,1,N,\"AV. JUSCELINO KUBTSCHEK\"");
-
-            saida.println("A80,365,0,1,1,1,N,\"Complemento\"");
-            saida.println("LO80,380,80,0");
-            saida.println("A80,390,0,3,1,1,N,\"ATE 951 LADO IMPAR\"");
-
-            saida.println("A360,365,0,1,1,1,N,\"Cep\"");
-            saida.println("LO360,380,80,0");
-            saida.println("A360,390,0,3,1,1,N,\"04543-000\"");
-
-            saida.println("A500,365,0,1,1,1,N,\"Coordenadas\"");
-            saida.println("LO500,380,80,0");
-            saida.println("A500,390,0,3,1,1,N,\"Pag:177 F25\"");
-
-            saida.println("P1");
-            saida.println("ETX");
             saida.close();
             writer.close();
 
@@ -136,5 +105,50 @@ public class NovoTesteLPT1 {
                     "Aviso!",
                     JOptionPane.ERROR_MESSAGE);
         }
+    }
+
+    private static void imprimeSaida(PrintWriter saida) {
+        saida.println("N");
+        saida.println("Q100,0");
+        saida.println("q5524");
+        saida.println("S5");
+        saida.println("D10");
+        saida.println("ZT");
+        saida.println("LO5,8,485,0");
+        saida.println("GG80,10, LOGORT1");
+        saida.println("I8,3,351");
+        saida.println("A5,10,0,1,1,1,N,\"Cooperativa   Mista   de   Trabalho  dos\"");
+        saida.println("A5,23,0,1,1,1,N,\"Motoristas  Autonomos  de  Taxi Especial\"");
+        saida.println("A5,36,0,1,1,1,N,\"de Sao Paulo - Radio-Taxi\"");
+        saida.println("A5,49,0,1,1,1,N,\"CNPJ:46.553.947/0001-20  CCM:8.179.517-3\"");
+        saida.println("A5,70,0,2,1,1,N,\"          VIA CONTABILIDADE        .\"");
+        saida.println("LO5,95,575,0");
+        saida.println("A5,110,0,2,1,2,N,\"4378315\"");
+        saida.println("B110,110,0,2,2,6,70,B,\"4378315\"");
+
+        saida.println("A10,230,0,1,1,1,N,\"Recepcionista\"");
+        saida.println("A200,230,0,1,1,1,N,\"Ponto de partida\"");
+        saida.println("LO10,255,130,0");
+        saida.println("LO200,255,160,0");
+        saida.println("A10,265,0,1,1,1,N,\"TATI\"");
+        saida.println("A200,265,0,1,1,1,N,\"Aeroporto de Congonhas\"");
+
+        saida.println("A10,295,0,1,1,1,N,\"Destino\"");
+        saida.println("LO10,310,80,0");
+        saida.println("A10,320,0,1,1,1,N,\"AV. JUSCELINO KUBTSCHEK\"");
+
+        saida.println("A10,365,0,1,1,1,N,\"Complemento\"");
+        saida.println("LO10,380,80,0");
+        saida.println("A10,390,0,1,1,1,N,\"ATE 951 LADO IMPAR\"");
+
+        saida.println("A290,365,0,1,1,1,N,\"Cep\"");
+        saida.println("LO290,380,80,0");
+        saida.println("A290,390,0,1,1,1,N,\"04543-000\"");
+
+        saida.println("A500,365,0,1,1,1,N,\"Coordenadas\"");
+        saida.println("LO500,380,80,0");
+        saida.println("A500,390,0,3,1,1,N,\"Pag:177 F25\"");
+
+        saida.println("P1");
     }
 }
